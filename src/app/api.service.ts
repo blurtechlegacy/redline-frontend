312a361fa4f5fdb382df2dc312218f90132c195d
Yuri Tsai <yura.tsai@gmail.com>
2019-05-19 13:12:30 +0700

    Changed backend address

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,17 +9,17 @@ import { AuthResponse } from './auth-response';
   providedIn: 'root'
 })
 export class ApiService {
-  url = 'http://api.redline.blur.tech';
+  url = 'https://redline-backend.herokuapp.com/';
   constructor(private http: HttpClient) { }
   getRoutes(city: string): Promise<Response> {
     const options = new HttpParams().set('city', city);
-    return this.http.get<Response>(this.url + '/routes', {params: options}).toPromise();
+    return this.http.get<Response>(this.url + 'routes', {params: options}).toPromise();
   }
   authorize(login: string, password: string): Observable<AuthResponse> {
     const body = {
       login, password
     };
     console.log(body);
-    return this.http.post<AuthResponse>(this.url + '/users/auth', body);
+    return this.http.post<AuthResponse>(this.url + 'users/auth', body);
   }
 }
